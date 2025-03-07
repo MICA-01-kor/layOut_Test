9d5885c1fb1129f023b394a6b714d4c111579267
MICA-01-kor <hoyoung2818@gmail.com>
2025-03-07 15:22:53 +0900

    Sample

---
+++ js/smaple.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".menu").hover(
+    function () {
+      $(this).find(".submenu").stop().slideDown();
+    },
+    function () {
+      $(".submenu").stop().slideUp();
+    }
+  );
+});
